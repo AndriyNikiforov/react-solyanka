67741dc1361673a262a1eb9e483541e4e871d3de
andriy.nikiforov <andriy.nikiforov.general@gmail.com>
2020-12-18 16:21:49 +0200

    Responsive fonts implement.

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import todos from '../../actions/todo';
 
@@ -40,19 +40,18 @@ class List extends React.Component {
     const { data, onClickComplete, onClickDelete } = this.props;
     const items = (data.todo.length > 0) ? data.todo.map((item) => (
       <li key={item.id}>
-        {item.content}
-        <br />
-        {item.title}
-        <br />
-        {item.status}
-        <br />
-        <Link to={`/edit/${item.id}`}>
-          Edit
-        </Link>
-        <br />
-        <input value="Complete" type="button" onClick={() => onClickComplete({ ...item, status: 'COMPLETED' })} />
-        <br />
-        <input value="Remove" type="button" onClick={() => onClickDelete(item.id)} />
+        <div className="li-content">
+          <h4>{item.title}</h4>
+          <p className="text">{item.content}</p>
+          <span className="label-status">{item.status}</span>
+        </div>
+        <div className="li-buttons">
+          <NavLink className="li-link" to={`/edit/${item.id}`}>
+            Edit
+          </NavLink>
+          <input value="Complete" type="button" onClick={() => onClickComplete({ ...item, status: 'COMPLETED' })} />
+          <input value="Remove" type="button" onClick={() => onClickDelete(item.id)} />
+        </div>
       </li>
     )) : (<></>);
 
