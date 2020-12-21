b9eda7b4c62172f9e2321ee9fda516051b23cb95
andriy.nikiforov <andriy.nikiforov.general@gmail.com>
2020-12-21 14:47:04 +0200

    Add proptypes validation.

--- src/components/PrivateRouter/index.jsx
+++ src/components/PrivateRouter/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Cookies from 'js-cookie';
 
 const PrivateRouter = ({ Component, path, exact }) => {
@@ -10,6 +11,18 @@ const PrivateRouter = ({ Component, path, exact }) => {
   ) : (
     <Redirect to={{ pathname: '/login' }} />
   );
+};
+
+PrivateRouter.propTypes = {
+  Component: PropTypes.func,
+  path: PropTypes.string,
+  exact: PropTypes.bool,
+};
+
+PrivateRouter.defaultProps = {
+  Component: <></>,
+  path: '/',
+  exact: false,
 };
 
 export default PrivateRouter;
